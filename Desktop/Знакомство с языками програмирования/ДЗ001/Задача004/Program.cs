7782608b35e1e216556d9f7426d19ad7bbac9ad2
Axe1 <bashkat.off.13@gmail.com>
2023-01-30 14:32:59 +0500

    Решил задачу № 2

---
+++ Desktop/Знакомство с языками програмирования/ДЗ001/Задача004/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+//2, 3, 7 -> 7
+//44 5 78 -> 78
+//22 3 9 -> 22
+
+Console.Write("Введите первое число для сравнения: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число для сравнения: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите третье число для сравнения: ");
+int c = Convert.ToInt32(Console.ReadLine());
+
+if (a > b && a > c)
+    {
+        Console.Write("Большее число: ");
+        Console.WriteLine(a); 
+    }  
+else if (b > a && b > c)
+    {
+        Console.Write("Большее число: ");
+        Console.WriteLine(b);
+    }   
+else if (c > a && c > b)
+    {
+        Console.Write("Большее число: ");
+        Console.WriteLine(c);
+    }
