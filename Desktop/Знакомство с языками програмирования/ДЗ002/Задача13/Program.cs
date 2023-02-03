fe5ebbbc7d46268ece94e4c89a1502002c2f8ea2
Axe1 <bashkat.off.13@gmail.com>
2023-02-03 22:21:57 +0500

    Решил Задачу №13 и добавил .gitignor

---
+++ Desktop/Знакомство с языками програмирования/ДЗ002/Задача13/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+//645 -> 5
+//78 -> третьей цифры нет
+//32679 -> 6
+
+Console.Clear();
+
+Console.WriteLine("Введите число: ");
+string num = Console.ReadLine();
+int n = num.Length;
+
+if (n >= 3)
+Console.WriteLine(num[2]);
+else
+Console.WriteLine("Третьей цифры нет");
