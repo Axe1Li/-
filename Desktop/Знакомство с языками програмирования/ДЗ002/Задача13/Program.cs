0881d6fdb43ec3e029dc81b5f9e7c4cd55191f9e
Axe1 <bashkat.off.13@gmail.com>
2023-02-06 12:25:33 +0500

    Переделал Задачу № 13

--- Desktop/Знакомство с языками програмирования/ДЗ002/Задача13/Program.cs
+++ Desktop/Знакомство с языками програмирования/ДЗ002/Задача13/Program.cs
@@ -9,6 +9,16 @@ Console.Clear();
 Console.WriteLine("Введите число: ");
 int n = Convert.ToInt32(Console.ReadLine());
 
-if (n >= 100 && n < 1000) {
-    n
-}
+if (n < 99)
+    Console.WriteLine("Третьей цифры нет!");
+
+while (n > 99) {
+    if (n < 1000 && n > 99) {
+        n = n % 10;
+        Console.WriteLine("Третье цифра в веденом числе: " + n);
+    }
+    else if(n > 999)
+        n = n / 10;
+}    
+
+
