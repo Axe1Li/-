d4c5848f5b97c804687ec7c917287b614306931c
Axe1 <bashkat.off.13@gmail.com>
2023-02-14 18:56:46 +0500

    Решил задачу №27

---
+++ Desktop/Знакомство с языками програмирования/ДЗ004/Задача27/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+//452 -> 11
+//82 -> 10
+//9012 -> 12
+
+Console.Clear();
+
+Console.Write("Введите многозначное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num < 10)
+    Console.WriteLine("Ошибка\nВведите многозначное число: ");
+int sum = 0;
+while (num != 0){
+    sum += num % 10;
+    num /= 10;
+}
+Console.WriteLine("Сумма цифр в числе ровна {0}", sum);
