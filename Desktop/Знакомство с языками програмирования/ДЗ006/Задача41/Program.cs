9825abc888a53bf50321300324729c48e97e46ff
Axe1 <bashkat.off.13@gmail.com>
2023-02-18 16:24:00 +0500

    Решил задачу №41

---
+++ Desktop/Знакомство с языками програмирования/ДЗ006/Задача41/Program.cs
@@ -0,0 +1,20 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+Console.Clear();
+
+Console.Write("Сколько чисел вы хотите ввести: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[num];
+for (int i = 0; i < array.Length; i++){
+    Console.WriteLine($"Введите {i+1} элемент: ");
+    array[i] = Convert.ToInt32(Console.ReadLine());
+}
+int El = 0;
+for (int i = 0; i < array.Length; i++){
+    if (array[i] > 0)
+        El ++; 
+}
+Console.WriteLine($"Чисел больше '0': {El}");
