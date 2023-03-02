acc7cabab43161af6429861cfc78c6c6a581407a
Axe1 <bashkat.off.13@gmail.com>
2023-03-02 16:54:54 +0500

    Решил задачу №43

--- Desktop/Знакомство с языками програмирования/ДЗ006/Задача43/Program.cs
+++ Desktop/Знакомство с языками програмирования/ДЗ006/Задача43/Program.cs
@@ -5,16 +5,20 @@
 // b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
 
 Console.Clear();
-Console.Write("Введите 1 число для первого уравнения: ");
-int b1 = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите 2 число для первого уравнения: ");
-int k1 = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите 1 число для второго уравнения: ");
-int b2 = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите 2 число для второго уравнения: ");
-int k2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите b1: ");
+double b1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите k1: ");
+double k1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите b2: ");
+double b2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите k2: ");
+double k2 = Convert.ToInt32(Console.ReadLine());
 
 
+// y = k1 * x + b1  =>  y = k1x + b1         =>  y = k1x + b2           =>  y = k1*(k1 - k2) + b1
+// y = k2 * x + b2  =>  k1x + b1 = k2x + b2  =>  (k1x - k2x) = b2 - b1  =>  x = (b2 - b1) / (k2 - k1)
 
+double x = (b2 - b1) / (k1 - k2);
+double y = k1 * x + b1;
 
-
+Console.WriteLine($"Точка персечения прямых: {(x, y)}");
