aa10f1da37d27f52b5ee84901e7a0b5af46c4ef7
Axe1 <bashkat.off.13@gmail.com>
2023-03-11 16:10:15 +0500

    Решил задачу№54

---
+++ Desktop/Знакомство с языками програмирования/ДЗ008/Задача№54/Program.cs
@@ -0,0 +1,57 @@
+﻿// //  Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию 
+// элементы каждой строки двумерного массива.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// В итоге получается вот такой массив:
+// 7 4 2 1
+// 9 5 3 2
+// 8 4 4 2
+
+void InputMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+            matrix[i, j] = new Random().Next(1, 21); // [1, 20]
+    }
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+            Console.Write($"{matrix[i, j]} \t");
+        Console.WriteLine();
+    }
+}
+void BubbleSort(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            for (int x = 0; x < matrix.GetLength(1) - 1; x++)
+            {
+                if (matrix[i, x] < matrix[i, x + 1])
+                {
+                    int temp = matrix[i, x];
+                    matrix[i, x] = matrix[i, x + 1];
+                    matrix[i, x + 1] = temp;
+                }
+            }
+        }
+    }
+}
+Console.Clear();
+Console.Write("Введите размер матрицы: ");
+int[] coord = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
+int[,] matrix = new int[coord[0], coord[1]];
+Console.WriteLine("Начальный массив");
+InputMatrix(matrix);
+PrintMatrix(matrix);
+BubbleSort(matrix);
+Console.WriteLine("Конечный массив");
+PrintMatrix(matrix);
