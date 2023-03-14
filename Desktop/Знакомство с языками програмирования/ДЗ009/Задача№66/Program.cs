2760acbdfa667e1ea73da1c09bb3b49ced499450
Башкатов Алексей <bashkat.off.13@gmail.com>
2023-03-14 23:31:59 +0500

    Решил задачу №66

---
+++ Desktop/Знакомство с языками програмирования/ДЗ009/Задача№66/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задайте значения M и N. Напишите программу, 
+// которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8 -> 30
+
+Console.Clear();
+
+Console.Write("Введите первое число: ");
+int M = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+while (M > N)
+{
+    Console.Write("Ошибка диапазона!\nВведите перове число: ");
+    M = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Введите второе число: ");
+    N = Convert.ToInt32(Console.ReadLine());
+}
+
+int Recursion(int M, int N)
+{
+    if (M == N) 
+    {
+        return N;
+    }
+    else 
+    {
+        return M + Recursion(M + 1, N);
+    }
+}
+Console.WriteLine($"Сумма натуральных чисел от {M} до {N} ровна: {Recursion(M, N)}");
